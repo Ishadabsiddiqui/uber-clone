a293f25f960e0a315585df15d50905b3fa969460
shadab <shadabsiddiqui@gmail.com>
2024-12-16 02:51:23 +0530

    user completed

---
+++ Backend/middlewares/auth.middleware.js
@@ -0,0 +1,18 @@
+const userModel = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+module.exports.authUser = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: "You are not authenticated." });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await userModel.findById(decoded._id);
+    req.user = user;
+    return next();
+  } catch (err) {
+    return res.status(401).json({ message: "You are not authenticated." });
+  }
+};
