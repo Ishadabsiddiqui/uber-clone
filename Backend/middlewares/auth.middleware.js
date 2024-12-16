5aa0fb30e40866c56532307ce077b353c9187a5d
shadab <shadabsiddiqui@gmail.com>
2024-12-17 00:46:26 +0530

    both user and captain authentication is Completed

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,13 +1,15 @@
 const userModel = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const blackListTokenModel = require("../models/blacklistToken.model");
+const captainModel = require("../models/captain.model");
 
 module.exports.authUser = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization;
   if (!token) {
     return res.status(401).json({ message: "You are not authenticated." });
   }
-  const isBlacklisted = await userModel.findOne({ token: token });
+  const isBlacklisted = await blackListTokenModel.findOne({ token: token });
   if (isBlacklisted) {
     return res.status(401).json({ message: "You are not authenticated." });
   }
@@ -17,6 +19,25 @@ module.exports.authUser = async (req, res, next) => {
     req.user = user;
     return next();
   } catch (err) {
+    return res.status(401).json({ message: "You are not authenticated." });
+  }
+};
+
+module.exports.authCaptain = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization?.split("")[1];
+  if (!token) {
+    return res.status(401).json({ message: "You are not authenticated." });
+  }
+  const isBlacklisted = await blackListTokenModel.findOne({ token: token });
+  if (isBlacklisted) {
+    return res.status(401).json({ message: "You are not authenticated." });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const captain = await captainModel.findById(decoded._id);
+    req.captain = captain;
+    return next();
+  } catch (error) {
     return res.status(401).json({ message: "You are not authenticated." });
   }
 };
