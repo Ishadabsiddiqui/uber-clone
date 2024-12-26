ee727d18693fc8f9eb295f07f7ca79fd05dc7096
shadab <shadabsiddiqui@gmail.com>
2024-12-27 01:04:39 +0530

    Project Route Completed

--- Backend/app.js
+++ Backend/app.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser");
 const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
 const captainRoutes = require("./routes/captain.routes");
+const mapsRoutes = require("./routes/maps.routes");
 
 connectToDb();
 
@@ -19,7 +20,8 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 app.use("/users", userRoutes);
-
 app.use("/captains", captainRoutes);
+
+app.use("/maps", mapsRoutes);
 
 module.exports = app;
