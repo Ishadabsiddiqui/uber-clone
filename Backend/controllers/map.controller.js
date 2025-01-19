025f8797edc9b45eb6b6198ae84bab4d8ff0624e
shadab <shadabsiddiqui@gmail.com>
2025-01-19 22:51:11 +0530

    coordinates completed

--- Backend/controllers/map.controller.js
+++ Backend/controllers/map.controller.js
@@ -16,3 +16,38 @@ module.exports.getCoordinates = async (req, res, next) => {
     res.status(404).json({ message: "Coordinates not found" });
   }
 };
+
+module.exports.getDistanceTime = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { origin, destination } = req.query;
+    const distanceTime = await mapService.getDistanceAndTime(
+      origin,
+      destination
+    );
+    res.status(200).json(distanceTime);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "Distance and time Coordinates not found" });
+  }
+};
+
+module.exports.getAutoCompleteSuggestions = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { input } = req.query;
+    const suggestions = await mapService.getSuggestions(input);
+    res.status(200).json(suggestions);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Autocomplete suggestions not found" });
+  }
+};
--- Backend/services/maps.service.js
+++ Backend/services/maps.service.js
@@ -8,6 +8,7 @@ module.exports.getAddressCoordinate = async (address) => {
 
   try {
     const response = await axios.get(url);
+    console.log(response);
 
     if (response.data.status === "OK") {
       const location = response.data.results[0].geometry.location;
@@ -18,6 +19,51 @@ module.exports.getAddressCoordinate = async (address) => {
       };
     } else {
       throw new Error("Unable to fetch coordinates");
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+module.exports.getDistanceAndTime = async (origin, destination) => {
+  if (!origin || !destination) {
+    throw new Error("Both origin and destination are required");
+  }
+  const apiKey = process.env.GOOGLE_MAPS_API;
+  const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${encodeURIComponent(
+    origin
+  )}&destinations=${encodeURIComponent(destination)}&key=${apiKey}`;
+  try {
+    const response = await axios.get(url);
+    if (response.data.status === "OK") {
+      if (response.data.rows[0].elements[0].status === "ZERO_RESULTS") {
+        throw new Error("Unable to find a location for the given address");
+      }
+      return response.data.rows[0].elements[0];
+    } else {
+      throw new Error("Unable to fetch distance and time");
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+module.exports.getSuggestions = async (input) => {
+  if (!input) {
+    throw new Error("Query is required");
+  }
+  const apiKey = process.env.GOOGLE_MAPS_API;
+  const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${encodeURIComponent(
+    input
+  )}&types=address&key=${apiKey}`;
+  try {
+    const response = await axios.get(url);
+    if (response.data.status === "OK") {
+      return response.data.predictions;
+    } else {
+      throw new Error("Unable to fetch suggestions");
     }
   } catch (error) {
     console.error(error);
